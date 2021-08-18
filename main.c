06b9c95bb0d75856d383c63113bd3cd33a8b5957
Adam Baumgartner <adamb@remarkablefoods.com>
2021-08-18 15:47:37 -0400

    blink working with debug - maybe

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-// This minimal Azure Sphere app repeatedly toggles an LED. Use this app to test that
-// installation of the device and SDK succeeded, and that you can build, deploy, and debug an app.
 
 #include <stdbool.h>
 #include <errno.h>
@@ -17,32 +15,19 @@ typedef enum {
 
 int main(void)
 {
-    int led1_r = GPIO_OpenAsOutput(LED1_R, GPIO_OutputMode_PushPull, GPIO_Value_High);
-    int led1_g = GPIO_OpenAsOutput(LED1_G, GPIO_OutputMode_PushPull, GPIO_Value_High);
-    int led1_b = GPIO_OpenAsOutput(LED1_B, GPIO_OutputMode_PushPull, GPIO_Value_High);
+    int led1= GPIO_OpenAsOutput(LED1_R, GPIO_OutputMode_PushPull, GPIO_Value_High);
 
-    if (led1_r < 0){
+    if (led1 < 0){
         return ExitCode_Main_Led;
     }
 
     const struct timespec sleepTime = {.tv_sec = 1, .tv_nsec = 0};
 
     while (true) {
-        GPIO_SetValue(led1_r, GPIO_Value_High);
-        GPIO_SetValue(led1_g, GPIO_Value_High);
-        GPIO_SetValue(led1_b, GPIO_Value_High);
+        Log_Debug("Hello World\n");
+        GPIO_SetValue(led1, GPIO_Value_High);
         nanosleep(&sleepTime, NULL);
-        GPIO_SetValue(led1_r, GPIO_Value_Low);
-        GPIO_SetValue(led1_g, GPIO_Value_High);
-        GPIO_SetValue(led1_b, GPIO_Value_High);
-        nanosleep(&sleepTime, NULL);
-        GPIO_SetValue(led1_r, GPIO_Value_High);
-        GPIO_SetValue(led1_g, GPIO_Value_Low);
-        GPIO_SetValue(led1_b, GPIO_Value_High);
-        nanosleep(&sleepTime, NULL);
-        GPIO_SetValue(led1_r, GPIO_Value_High);
-        GPIO_SetValue(led1_g, GPIO_Value_High);
-        GPIO_SetValue(led1_b, GPIO_Value_Low);
+        GPIO_SetValue(led1, GPIO_Value_Low);
         nanosleep(&sleepTime, NULL);
     }
 }
